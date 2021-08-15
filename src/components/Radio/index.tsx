61c0c95116da5095d80d347b12cfd9f9b9d475b2
Jaehoo Im <im.jaehoo1@gmail.com>
2021-08-16 06:35:30 +0900

    <feat> update form

---
+++ src/components/Radio/index.tsx
@@ -0,0 +1,8 @@
+import { JSX } from 'solid-js';
+import { styled } from 'solid-styled-components';
+
+interface RadioProps {
+  options: { value: string | number; text: string }[];
+}
+
+const Radio = (props: RadioProps) => {};
